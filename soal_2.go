de433254b66e24e5097895a29fa725f367add03c
mongol3 <ariesekoprasetyo@outlook.com>
2023-05-03 20:33:11 +0700

    [FIX]

--- soal_2.go
+++ soal_2.go
@@ -8,16 +8,21 @@ import (
 
 var wg sync.WaitGroup
 var mutex sync.Mutex
+var flag bool
 
-func procLoop1(dat1 []interface{}, wg *sync.WaitGroup) {
-	for i := 1; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println(dat1, i)
-
+func procLoop1(dat1 []interface{}, isEven bool) {
+	for i := 0; i < 5; i++ {
+		mutex.Lock()
+		if flag == isEven {
+			fmt.Println(dat1, i)
+			flag = !isEven
+		}
+		mutex.Unlock()
+		time.Sleep(500 * time.Millisecond)
 	}
-
 	wg.Done()
 }
+
 func main() {
 	data1 := []interface{}{
 		"Coba1", "Coba2", "Coba3",
@@ -26,14 +31,8 @@ func main() {
 		"Bisa1", "Bisa2", "Bisa3",
 	}
 	wg.Add(2)
-
-	mutex.Lock()
-	go procLoop1(data1, &wg)
-	mutex.Unlock()
-
-	mutex.Lock()
-	go procLoop1(data2, &wg)
-	mutex.Unlock()
-
+	flag = false
+	go procLoop1(data1, true)
+	go procLoop1(data2, false)
 	wg.Wait()
 }
